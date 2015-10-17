929738e74cbf8fca454d8ffbac6a0ce1a1a0052f
alanquigley <alan.quigley@gmail.com>
2015-10-17 13:57:40 +0100

    added folders

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,14 @@
  */
 angular
     .module('angular-buildup', [])
+
     .run(function() {
         console.log('Angular app have runned');
-    });
+    })
+
+    .config(['$routeProvider',function() {
+      $routeProvider.when('/', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      });
+    }])
