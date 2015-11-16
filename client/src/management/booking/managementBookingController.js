bc71a2073b5ae88dbd7d579879d4bb59f85d9c7c
leticiacalmon <leti.calmon@gmail.com>
2015-11-16 15:40:20 +0000

    Listing and edit room

--- client/src/management/booking/managementBookingController.js
+++ client/src/management/booking/managementBookingController.js
@@ -2,9 +2,13 @@ ManagementBookingController.$inject = ['ManagementBookingService'];
 
 function ManagementBookingController(managementBookingService){
   var vm = this;
+  var i = 0;
 
   managementBookingService.query(function(data){
     vm.bookings = data.data;
+    for(i in vm.bookings) {
+      vm.bookings[i].nights = daydiff(new Date(vm.bookings[i].startDate), new Date(vm.bookings[i].endDate));
+    }
   },function (){
     alert('error retrieving booking!');
   });
@@ -12,6 +16,22 @@ function ManagementBookingController(managementBookingService){
   function daydiff(startDate , endDate){
       return Math.round((endDate - startDate)/(1000*60*60*24));
   }
+
+  vm.edit = function (bookingID){
+    managementBookingService.get(bookingID, function (data) {
+      vm.booking = data.data;
+    },function (){
+      alert('error retrieving booking!');
+    });
+  };
+
+  vm.cancel = function (bookingID){
+    managementBookingService.remove(bookingID, function () {
+      alert('success!');
+    },function (){
+      alert('error canceling booking!');
+    });
+  };
 }
 
 app.controller('ManagementBookingController', ManagementBookingController);
--- client/src/room/roomController.js
+++ client/src/room/roomController.js
@@ -1,48 +1,48 @@
 
 RoomController.$inject = ['RoomService', 'RoomTypeService', 'RoomGuestService', 'BookingService'];
 function RoomController(roomService, roomTypeService, roomGuestService, bookingService) {
-    var room = this;
+  var room = this;
 
-    roomService.query(function (data) {
-        room.rooms = data.data;
+  roomService.query(function (data) {
+    room.rooms = data.data;
+  }, function () {
+    alert('error!');
+  });
+
+  room.types = roomTypeService.getAllTypes();
+  room.guests = roomGuestService.getAllGuests();
+
+  room.search = function () {
+    roomService.query(room.filter, function (data) {
+      room.rooms = data.data;
     }, function () {
-        alert('error!');
+      alert('error!');
     });
+  };
 
-    room.types = roomTypeService.getAllTypes();
-    room.guests = roomGuestService.getAllGuests();
+  function daydiff(startDate , endDate){
+    return Math.round((endDate - startDate)/(1000*60*60*24));
+  }
 
-    room.search = function () {
-      roomService.query(room.filter, function (data) {
-          room.rooms = data.data;
-      }, function () {
-          alert('error!');
+  room.book = function (form, selectedRoom) {
+    room.bookClicked = true
+    if (form.$valid) {
+      
+      bookingService.save({
+        roomID: selectedRoom.roomID,
+        guests: room.filter.capacity,
+        doubleBed: selectedRoom.doubleBed,
+        standard: selectedRoom.standard,
+        startDate: room.booking.startDate,
+        endDate: room.booking.endDate,
+        bookingHolderName: room.booking.holderName,
+        bookingHolderEmail: room.booking.holderEmail,
+        pricePerNight: selectedRoom.price,
+        amount: selectedRoom.price*daydiff(room.booking.startDate, room.booking.endDate)
       });
-    };
-
-    function daydiff(startDate , endDate){
-        return Math.round((endDate - startDate)/(1000*60*60*24));
+    } else {
+      alert('Fill all Fields');
     }
-
-    room.book = function (form, selectedRoom) {
-        room.bookClicked = true
-        if (form.$valid) {
-
-            bookingService.save({
-                roomID: selectedRoom.roomID,
-                guests: room.filter.capacity,
-                doubleBed: selectedRoom.doubleBed,
-                standard: selectedRoom.standard,
-                startDate: room.booking.startDate,
-                endDate: room.booking.endDate,
-                bookingHolderName: room.booking.holderName,
-                bookingHolderEmail: room.booking.holderEmail,
-                pricePerNight: selectedRoom.price,
-                amount: selectedRoom.price*daydiff(room.filter.startDate, room.filter.endDate)
-            });
-        } else {
-            alert('Fill all Fields');
-        }
-    }
+  }
 }
 app.controller('RoomController', RoomController);
